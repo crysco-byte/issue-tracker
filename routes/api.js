380f05b5303f3b70c2b58f220fc69914b8de612e
cysco-byte <cryptoescor@protonmail.com>
2021-02-16 18:20:31 -0300

    final commit

--- routes/api.js
+++ routes/api.js
@@ -1,27 +1,146 @@
-'use strict';
+"use strict";
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const issueSchema = new mongoose.Schema({
+  issue_title: { type: String, required: true },
+  issue_text: { type: String, required: true },
+  created_by: { type: String, required: true },
+  assigned_to: String,
+  status_text: String,
+  open: Boolean,
+  created_on: String,
+  updated_on: String,
+  project: String,
+});
+
+const issueModel = mongoose.model("issueModel", issueSchema);
 
 module.exports = function (app) {
+  app
+    .route("/api/issues/:project")
 
-  app.route('/api/issues/:project')
-  
-    .get(function (req, res){
+    .get(function (req, res) {
       let project = req.params.project;
-      
+
+      let queryObj = {
+        ...((req.query._id == undefined ? false : true) && {
+          _id: req.query._id,
+        }),
+        ...((req.query.issue_title == undefined ? false : true) && {
+          issue_title: req.query.issue_title,
+        }),
+        ...((req.query.issue_text == undefined ? false : true) && {
+          issue_text: req.query.issue_text,
+        }),
+        ...((req.query.created_by == undefined ? false : true) && {
+          created_by: req.query.created_by,
+        }),
+        ...((req.query.assigned_to == undefined ? false : true) && {
+          assigned_to: req.query.assigned_to,
+        }),
+        ...((req.query.status_text == undefined ? false : true) && {
+          status_text: req.query.status_text,
+        }),
+        ...((req.query.created_on == undefined ? false : true) && {
+          created_on: req.query.created_on,
+        }),
+        ...((req.query.updated_on == undefined ? false : true) && {
+          updated_on: req.query.updated_on,
+        }),
+        ...((req.query.open == undefined ? false : true) && {
+          open: req.query.open,
+        }),
+        project: project,
+      };
+      issueModel.find(queryObj, (err, doc) => {
+        if (err) res.send(err);
+        res.send(doc);
+      });
     })
-    
-    .post(function (req, res){
+
+    .post(function (req, res) {
       let project = req.params.project;
-      
+      const {
+        issue_title,
+        issue_text,
+        created_by,
+        assigned_to,
+        status_text,
+      } = req.body;
+
+      issueModel.create(
+        {
+          issue_title: issue_title,
+          issue_text: issue_text,
+          created_by: created_by,
+          assigned_to: assigned_to,
+          status_text: status_text,
+          open: true,
+          created_on: new Date().toISOString(),
+          updated_on: new Date().toISOString(),
+          project: project,
+        },
+        (err, doc) => {
+          res.json({
+            _id: doc._id,
+            issue_title: doc.issue_title,
+            issue_text: doc.issue_text,
+            created_by: doc.created_by,
+            assigned_to: doc.assigned_to,
+            status_text: doc.status_text,
+            open: doc.open,
+            created_on: doc.created_on,
+            updated_on: doc.updated_on,
+          });
+        }
+      );
     })
-    
-    .put(function (req, res){
+
+    .put(async function (req, res) {
       let project = req.params.project;
-      
+      const {
+        _id,
+        issue_title,
+        issue_text,
+        created_by,
+        assigned_to,
+        status_text,
+        open,
+      } = req.body;
+      let updateObjs = {
+        ...((issue_title == "" ? false : true) && {
+          issue_title: issue_title,
+        }),
+        ...((issue_text == "" ? false : true) && {
+          issue_text: issue_text,
+        }),
+        ...((created_by == "" ? false : true) && {
+          created_by: created_by,
+        }),
+        ...((assigned_to == "" ? false : true) && {
+          assigned_to: assigned_to,
+        }),
+        ...((status_text == "" ? false : true) && {
+          status_text: status_text,
+        }),
+        ...((open == undefined ? false : true) && { open: false }),
+      };
+      let result = await issueModel.update({ _id: _id }, updateObjs);
+
+      result.n === 1
+        ? res.json({ result: "successfully updated", _id: _id })
+        : null;
     })
-    
-    .delete(function (req, res){
+
+    .delete(async function (req, res) {
       let project = req.params.project;
-      
+      const { _id } = req.body;
+      let result = await issueModel.deleteOne({ _id: _id });
+      result.n === 1
+        ? res.send({ result: "successfully deleted", _id: _id })
+        : null;
     });
-    
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,10 +1,63 @@
-const chaiHttp = require('chai-http');
-const chai = require('chai');
+const chaiHttp = require("chai-http");
+const chai = require("chai");
 const assert = chai.assert;
-const server = require('../server');
+const server = require("../server");
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
-  
+suite("Functional Tests", function () {
+  test("Crete an issue", (done) => {
+    chai
+      .request(server)
+      .post("/api/issues/apitest")
+      .type("form")
+      .send({
+        issue_title: "functional test",
+        issue_text: "test this function test testing",
+        created_by: "author test",
+        assigned_to: "someone",
+        status_text: "in Development",
+      })
+      .end((err, res) => {
+        assert.equal(res.body.issue_title, "functional test");
+        assert.equal(res.body.issue_text, "test this function test testing");
+        assert.equal(res.body.created_by, "author test");
+        assert.equal(res.body.assigned_to, "someone");
+        assert.equal(res.body.status_text, "in Development");
+        done();
+      });
+  });
+  test("Get issue", (done) => {
+    chai
+      .request(server)
+      .get("/api/issues/apitest")
+      .query({
+        issue_title: "Get Test",
+        issue_text: "unique text",
+        created_by: "test case",
+      })
+      .end((err, res) => {
+        assert.isOk(res.body);
+        done();
+      });
+  });
+  test("Update Doc", (done) => {
+    chai
+      .request(server)
+      .put("/api/issues/apitest")
+      .send({
+        _id: "602c173739c34e7cf0839b13",
+        issue_title: "updated",
+        issue_text: "updated",
+        created_by: "test case",
+        assigned_to: "tester",
+        status_text: "in Development",
+        open: false,
+      })
+      .end((err, res) => {
+        assert.equal(res.body.result, "successfully updated");
+        assert.equal(res.body._id, "602c173739c34e7cf0839b13");
+        done();
+      });
+  });
 });
